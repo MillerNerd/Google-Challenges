d30c0565928b73d26cc406f40fdec76f49889886
Sean <millernerd@gmail.com>
2020-07-11 14:11:43 -0500

    Fixed expected/actual order in tests

--- lovely-lucky-lambs/LAMBs_test.py
+++ lovely-lucky-lambs/LAMBs_test.py
@@ -4,61 +4,61 @@ from LAMBs import stingy, lambs, generous
 
 class MyTestCase(unittest.TestCase):
     def test_stingy_1(self):
-        self.assertEqual(stingy(1), 1)
+        self.assertEqual(1, stingy(1))
 
     def test_stingy_2(self):
-        self.assertEqual(stingy(2), 2)
+        self.assertEqual(2, stingy(2))
 
     def test_stingy_3(self):
-        self.assertEqual(stingy(3), 2)
+        self.assertEqual(2, stingy(3))
 
     def test_stingy_4(self):
-        self.assertEqual(stingy(4), 3)
+        self.assertEqual(3, stingy(4))
 
     def test_stingy_10(self):
-        self.assertEqual(stingy(10), 4)
+        self.assertEqual(4, stingy(10))
 
     def test_stingy_19(self):
-        self.assertEqual(stingy(19), 5)
+        self.assertEqual(5, stingy(19))
 
     def test_stingy_20(self):
-        self.assertEqual(stingy(20), 6)
+        self.assertEqual(6, stingy(20))
 
     def test_stingy_231(self):
-        self.assertEqual(stingy(231), 10)
+        self.assertEqual(10, stingy(231))
 
     def test_stingy_232(self):
-        self.assertEqual(stingy(232), 11)
+        self.assertEqual(11, stingy(232))
 
     def test_stingy_143(self):
-        self.assertEqual(stingy(143), 10)
+        self.assertEqual(10, stingy(143))
 
     def test_generous_1(self):
-        self.assertEqual(generous(1), 1)
+        self.assertEqual(1, generous(1))
 
     def test_generous_2(self):
-        self.assertEqual(generous(2), 1)
+        self.assertEqual(1, generous(2))
 
     def test_generous_3(self):
-        self.assertEqual(generous(3), 2)
+        self.assertEqual(2, generous(3))
 
     def test_generous_4(self):
-        self.assertEqual(generous(4), 2)
+        self.assertEqual(2, generous(4))
 
     def test_generous_126(self):
-        self.assertEqual(generous(126), 6)
+        self.assertEqual(6, generous(126))
 
     def test_generous_127(self):
-        self.assertEqual(generous(127), 7)
+        self.assertEqual(7, generous(127))
 
     def test_generous_128(self):
-        self.assertEqual(generous(128), 7)
+        self.assertEqual(7, generous(128))
 
     def test_lambs_143(self):
-        self.assertEqual(lambs(143), 3)
+        self.assertEqual(3, lambs(143))
 
     def test_lambs_10(self):
-        self.assertEqual(lambs(10), 1)
+        self.assertEqual(1, lambs(10))
 
 
 if __name__ == '__main__':
