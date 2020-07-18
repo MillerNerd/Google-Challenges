a662859a22d981e4f8fa7d23e8f8231d13dcf094
Sean <millernerd@gmail.com>
2020-07-17 21:16:27 -0500

    Added case for 3

--- fuel-injection-perfection/solution.py
+++ fuel-injection-perfection/solution.py
@@ -1,13 +1,15 @@
 def solution(num):
     count = 0
     while num > 1:
+        if num == 3:
+            return count + 2
         # if even, half
         if num % 2 == 0:
             count = count + 1
             num = num / 2
         else:
             # nearest num divisible by 4 is most efficient
-            # this isn't always true
+            # this isn't true for 3
             if (num + 1) % 4 == 0:
                 num = num + 1
                 count = count + 1
