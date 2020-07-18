ab92eede5d66d119defc9715039e3c28672a44ae
Sean <millernerd@gmail.com>
2020-07-17 21:13:51 -0500

    Comment

--- fuel-injection-perfection/solution.py
+++ fuel-injection-perfection/solution.py
@@ -6,6 +6,7 @@ def solution(num):
             count = count + 1
             num = num / 2
         else:
+            # nearest num divisible by 4 is most efficient
             if (num + 1) % 4 == 0:
                 num = num + 1
                 count = count + 1
