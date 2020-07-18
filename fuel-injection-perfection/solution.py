969b7bb1438997f0c21bab611b0f1b84cfbba6ac
Sean <millernerd@gmail.com>
2020-07-17 21:13:09 -0500

    Partially functional

--- fuel-injection-perfection/solution.py
+++ fuel-injection-perfection/solution.py
@@ -1,2 +1,24 @@
-def solution():
-    pass
+def solution(num):
+    count = 0
+    while num > 1:
+        # if even, half
+        if num % 2 == 0:
+            count = count + 1
+            num = num / 2
+        else:
+            if (num + 1) % 4 == 0:
+                num = num + 1
+                count = count + 1
+            else:
+                num = num - 1
+                count = count + 1
+    return count
+
+
+# def even(num):
+#     if num == 1:
+#         return
+#
+#
+# def odd(num):
+#     pass
