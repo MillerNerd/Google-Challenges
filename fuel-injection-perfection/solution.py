97d29448980088fe025cbe87fa8430f52936160c
Sean <millernerd@gmail.com>
2020-07-17 21:47:34 -0500

    Alternate solution

--- fuel-injection-perfection/solution.py
+++ fuel-injection-perfection/solution.py
@@ -1,23 +1,18 @@
-def solution(num):
+def solution(n):
     count = 0
-    while num > 1:
-        if num == 3:
+    while n > 1:
+        if n == 3:
             return count + 2
         # if even, half
-        if num % 2 == 0:
+        if n % 2 == 0:
             count = count + 1
-            num = num / 2
+            n = n / 2
         else:
             # nearest num divisible by 4 is most efficient
             # this isn't true for 3
-            if (num + 1) % 4 == 0:
-                num = num + 1
-                count = count + 1
-            else:
-                num = num - 1
-                count = count + 1
+            n = round(n/4.0)
+            count = count + 3
     return count
-
 
 # def even(num):
 #     if num == 1:
